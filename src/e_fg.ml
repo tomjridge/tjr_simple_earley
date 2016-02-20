19f776b62773b23471be71c8396acd738e9a6538
Tom Ridge <tom.j.ridge@googlemail.com>
2016-02-20 10:31:57 +0000

    +

--- src/e_fg.ml
+++ src/e_fg.ml
@@ -262,15 +262,39 @@ let earley_as_list c0 nt = (
 )
 *)
 
-let _ = str := (String.make 200 '1')  
+let _ = str := (String.make 100 '1')  
 let _ = earley (c0 ()) e'
 let _ = print_endline "Finished"
     
-(* Sample time for string length 200: 
+(* 
+Sample time for string length 200: 
 Finished
 
 real	0m0.958s
 user	0m0.950s
 sys	0m0.008s
+
+
+For length 400:
+Finished
+
+real	0m8.228s
+user	0m8.219s
+sys	0m0.012s
+
+500:
+Finished
+
+real	0m16.456s
+user	0m16.411s
+sys	0m0.048s
+
+600:
+Finished
+
+real	0m28.890s
+user	0m28.842s
+sys	0m0.060s
+
 
 *)
