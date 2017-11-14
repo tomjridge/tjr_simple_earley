6b6b5c5ec7dddd6b53133434b8cf01e000c2c728
Tom Ridge <tom.j.ridge@googlemail.com>
2017-11-14 11:57:08 +0000

    +

--- src/bash_env.sh
+++ src/bash_env.sh
@@ -4,19 +4,12 @@ set -a # export all vars
 libname="tjr_simple_earley"
 package_name="tjr_simple_earley"
 
-ocamlc="ocamlfind ocamlc -g -w @f@p@u@s@40-8-11-26  "
-ocamlopt="ocamlfind ocamlopt -g -w @f@p@u@s@40-8-11-26  "
+ocamlc="ocamlfind ocamlc -g   "
+ocamlopt="ocamlfind ocamlopt -g   "
 
 mls="profile_waypoints.ml profile.ml tjr_earley.ml"
 cmos="profile_waypoints.cmo profile.cmo tjr_earley.cmo"
 cmxs="profile_waypoints.cmx profile.cmx tjr_earley.cmx"
 
-mk_cmxa="ocamlfind ocamlopt -g -w @f@p@u@s@40-8-11-26  "
-
-
-function clean() {
-  rm -f *.cmi *.cmo *..cmx *.o *.x *.a *.cma *.cmxa
-	find . -xtype l -exec rm -f \{\} \;
-}
-
+mk_cmxa="ocamlfind ocamlopt -g   "
 
