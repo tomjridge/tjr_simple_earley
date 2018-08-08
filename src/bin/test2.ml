1753a632bbe180d3cac8e607c8c82c489ecb1100
Tom Ridge <tom.j.ridge@googlemail.com>
2018-08-08 12:18:04 +0100

    +

--- src/bin/test2.ml
+++ src/bin/test2.ml
@@ -19,7 +19,7 @@ module Make_sym(X:sig type sym val is_nt: sym -> bool end) = struct
   let is_even x = x mod 2 = 0
   let is_nt: int -> bool = is_even
 
-  let make = 
+  let make () = 
     let enum = { free=0; tbl=Hashtbl.create 10 } in
     let sym2int = fun s -> 
       try
@@ -50,54 +50,46 @@ module Make_rhs(X:sig type sym end) = struct
      more than once per nonterm per parse *)
   type rhs_enum = {
     mutable free:int;
-    tbl: (sym list,(int * int option)) Hashtbl.t  (* mutable anyway *)
+    bs2i: (sym list,int) Hashtbl.t;  (* mutable anyway *)
+    hd_bs: int option array 
   }
 
   let make ~sym2int = 
-    let enum = { free=0; tbl=Hashtbl.create 10 } in
-    let rhs2ii = fun rhs -> 
+    let enum = { free=0; bs2i=Hashtbl.create 10; hd_bs=Array.make 100 None} in
+    let rhs2i = fun rhs -> 
       try
-        Hashtbl.find enum.tbl rhs
+        Hashtbl.find enum.bs2i rhs
       with _ -> 
         (* if we don't already have it, we add rhs and all suffixes of rhs *)
         let rec loop rhs = match rhs with
           | [] -> (
               let n = enum.free in
-              Hashtbl.add enum.tbl rhs (n,None);
+              Hashtbl.add enum.bs2i rhs n;
+              (* leave hd_bs as None *)
               enum.free <- n +1;
+              (* Printf.printf "free %d\n%!" enum.free; *)
               ())
           | sym::rest -> (
               let n = enum.free in
-              Hashtbl.add enum.tbl rhs (n,sym2int sym);
+              Hashtbl.add enum.bs2i rhs n;
+              Array.set enum.hd_bs n (Some (sym2int sym));
               enum.free <- n +1;
+              (* Printf.printf "free %d\n%!" enum.free; *)
               loop rest)
         in
         loop rhs;
-        Hashtbl.find enum.tbl rhs
+        Hashtbl.find enum.bs2i rhs
     in
-    fun f -> f ~rhs2ii
+    (* NOTE hd_bs takes bs as an *int* *)
+    let hd_bs = fun rhs_n -> 
+      (* Printf.printf "rhs_n %d\n%!" rhs_n; *)
+      Array.get enum.hd_bs rhs_n in
+    fun f -> f ~rhs2i ~hd_bs
 
 end
 
 
-  (* itms (without start index i) are triples
-     (nt,bs_as_int,int_opt_hd_bs); FIXME calculate int_opt using
-     lookup table instead?
-
-     perhaps we should be working with itms rather than enumerated
-     rhs, since we also want to enumerate itms
-
-
-     something like to_int(nt,i,bs_as_int) with the understanding that
-     we can take the tl of bs by incrememting by 1
-
-     but we still need to do something for hd_bs; this is basically an
-     int -> int option map where the src and dst are small; so
-     presumably we should use an array
-
-  *)
-
-FIXME got here
+  (* itms are triples (nt,i,bs_as_int)  *)
 
 open Tjr_earley
 open Set_ops
@@ -144,73 +136,19 @@ module S = struct
      we also need to be able to take the tl of a list of bs
   *)
 
-  (* all tails of a list, tails of tails etc *)
-  let rec tails = function
-    | [] -> [[]]
-    | _::xs' as xs -> xs::tails xs'
+  type rhs = int
 
-  type rhs = sym list
-
-(* The idea is that for an item X -> i,as,k,bs we need to number all
-   the possible bs *)
-
-  (* construct a table for a bijection between suffixes of right hand
-     sides and ints *)
-  (* xs are right-hand sides; every suffix must be numbered *)
-  let mk_table xs : rhs array =
-    let count = ref 0 in
-    let arr = Array.make 100 [] in  (* array of sym_list *)  (* FIXME 100 *)
-    List.iter 
-      (fun x ->
-         (* x is a rhs, so take all suffixes *)
-         tails x |> List.iter (fun suff -> arr.(!count) <- suff; count:=!count+1))
-      xs
-    ;
-    Printf.printf "Right hand side suffix count: %d\n" !count;
-    arr
-
-  let _ = mk_table
-
-  (* scan array entries till property holds; return arr index; FIXME move to tjr_lib *)
-  let scan arr p = 
-    let rec f n = 
-      if n >= Array.length arr then None else
-        if p (arr.(n)) then Some n else f (n+1)
-    in
-    f 0
-
-  (* lookup a particular (suffix of) a rhs in the array; return the
-     index *)
-  (* NOTE this is potentially wrong if we have multiple nt with the
-     same rhs, because we pick out a rhs for the "wrong" nt *)
-  (* no need for a fix: the semantics is that each rhs has a number,
-     and that incrementing the number (if non-[]) gives the suffix;
-     don't need to link to nt FIXME introduce a better interface to
-     make this semantics clear; don't access arr directly; perhaps
-     call this a "rhs_numbering" *)
-  let lookup arr rhs =
-    scan arr (fun x -> x=rhs)
-
-  let lookup_NOTE_SLOW = lookup
-
-  (*
-  let _arr = mk_table [[1;1;1]]
-
-  let _tmp = lookup _arr [1]
-  *)
-
-  (* We want to represent items as ints; b is a "base" *)
-  (* nt,i,k,bs - repn as int, nt*b^3 + i*b^2 + k*b^1 + bs? *)
-  type nt_item = int
+  type nt_item = int  (*  nt * int_i * int_k * int_rhs *)
 
   (* Here we choose b to be 1024; obviously this places a limit on the
-     number of grammar; for larger grammars we need larger b *)
+     number of rules; for larger grammars we need larger b *)
   let b1 = 1024 
   let b2 = b1 * 1024 
   let b3 = b2 * 1024 
 
   (* Encode an item (omitting the "as" component) as an int *)
   let to_int (nt,i,k,bs) = 
+    (* Printf.printf "to_int %d %d %d %d\n%!" nt i k bs; *)
     nt*b3 + i*b2 + k*b1 + bs
     
   (* not needed?
@@ -225,24 +163,14 @@ module S = struct
   let dot_i nitm = (nitm / b2) mod b1
   let dot_k nitm = (nitm / b1) mod b1
   let dot_bs_as_int nitm = nitm mod b1
-  let dot_bs' (arr:sym list array) nitm = dot_bs_as_int nitm |> fun x -> arr.(x)  (* notice that we need the array to get the actual list *)
 
-  (* FIXME do we want to pass an aux data around for dot_bs and cut? no, better to pass operations at runtime *)
-
-(*
-  let _nitm = mk_nt_item _arr 3 4 5 [1]
-
-  let _nt = _nitm |> dot_nt
-  let _i,_k,_bs = (_nitm|>dot_i),(_nitm|>dot_k),(_nitm|>dot_bs' _arr)
-*)
-
-  (* NOTE following doesn't actually depend on arr *)
-  (* Implement the "cut" operation using arithmetic operations
-     only. *)
   let cut : nt_item -> j_t -> nt_item = 
-    let from_int nitm = (dot_nt nitm,dot_i nitm, dot_k nitm, dot_bs_as_int nitm) in
+    let from_int nitm = 
+      (dot_nt nitm,dot_i nitm, dot_k nitm, dot_bs_as_int nitm) |> fun (nt,i,k,bs) ->
+      (* Printf.printf "from_int %d %d %d %d\n%!" nt i k bs; *)
+      (nt,i,k,bs)
+    in
     fun bitm j0 -> 
-      (*        let as_ = (List.hd bitm.bs)::bitm.as_ in*)
       let (nt,i,k,bs) = from_int bitm in
       let bs = bs+1 in (* NOTE artefact of the numbering *)
       let k = j0 in
@@ -255,7 +183,7 @@ module S = struct
     dot_nt: nt_item -> nt;
     dot_i: nt_item -> i_t;
     dot_k: nt_item -> k_t;
-    dot_bs: nt_item -> sym list;
+    dot_bs_hd: nt_item -> sym option;
   }
 
   (* The rest of the code is straightforward *)
@@ -311,26 +239,32 @@ open S
 (* Encode nonterminals and terminals as ints; nts are even; tms are
    odd *)
 
-let _E = 0
-let eps = 1
-let _1 = 3
+type sym = E | One | Eps
+
+let is_nt = function | E -> true | _ -> false
+
+module X1 = struct type nonrec sym = sym let is_nt = is_nt end
+
+module Tmp1 = Make_sym(X1)
+
+let sym2int : sym -> int = Tmp1.make () @@ fun ~sym2int -> sym2int
+
+module Tmp2 = Make_rhs(X1)
 
 (* Encode the grammar E -> E E E | "1" | eps *)
-let rhss = [ [_E;_E;_E]; [_1]; [eps] ]
+let rhss = [ [E;E;E]; [One]; [Eps] ]
 
-let arr : int list array = S.mk_table rhss
-
-(* let rhss = rhss |> List.map @@ S.lookup arr *)
-
-let rhss' = rhss |> List.map @@ fun  x-> 
-  S.lookup arr x |> function Some x -> x | None -> failwith __LOC__
-
+let (rhs2i,hd_bs) = Tmp2.make ~sym2int @@ fun ~rhs2i ~hd_bs -> (rhs2i,hd_bs)
 
 (* Provide a function that produces new items, given a nonterminal and
    an input position k *)
-let new_items ~nt ~input ~k = match () with
-  | _ when nt = _E -> 
-    rhss'   (* E -> E E E | "1" | eps *)
+
+let rhss' = List.map rhs2i rhss
+
+let new_items ~nt ~input ~k = 
+  match nt with
+  | _ when nt = sym2int E -> 
+    rhss'
     |> List.map (fun bs -> let i = k in S.to_int (nt,i,k,bs))
   | _ -> failwith __LOC__
 
@@ -341,8 +275,8 @@ let input = String.make (Sys.argv.(1) |> int_of_string) '1'
    position k in the input *)
 let parse_tm ~tm ~input ~k ~input_length = 
   match () with
-  | _ when tm = eps -> [k]
-  | _ when tm = _1 -> 
+  | _ when tm = sym2int Eps -> [k]
+  | _ when tm = sym2int One -> 
     (* print_endline (string_of_int k); *)
     if String.get input k = '1' then [k+1] else []
   | _ -> failwith __LOC__
@@ -350,16 +284,17 @@ let parse_tm ~tm ~input ~k ~input_length =
 let input_length = String.length input
 
 (* Initial nonterminal *)
-let init_nt = _E
+let init_nt = sym2int E
+
+let dot_bs_hd nitm = 
+  nitm |> S.dot_bs_as_int |> hd_bs
 
 (* Construct (nonterminal) item operations *)
-let dot_bs = dot_bs' arr
-
 let nt_item_ops = {
   dot_nt;
   dot_i;
   dot_k;
-  dot_bs
+  dot_bs_hd
 }
 
 let bitms_lt_k_ops = {
@@ -377,230 +312,21 @@ let main () =
   run_earley ~nt_item_ops ~bitms_lt_k_ops ~cut ~new_items ~input ~parse_tm ~input_length ~init_nt 
   |> fun s -> s.k |> string_of_int |> print_endline
 
-let _ = main ()
+let _ = main () 
 
 (* 
 
-$ src $ time ./test.native 400
-Right hand side suffix count: 8
+$ bin $ time ./test2.native 400
 400
 
-real	0m2.491s
+real	0m2.508s
 user	0m2.484s
-sys	0m0.004s
+sys	0m0.016s
+
+# (h:pc1177) (p:~/l/github/p_tjr_simple_earley/src/bin) (d:/dev/loop7[/git])  [dev !?]
 
 *)
 
 
 
 
-
-(* FIXME separate these out so that they can be tested independently
-
-(* S -> S S "x" | "x" ----------------------------------------------- *)
-
-module G2 = struct
-
-  open S
-
-  (* terminals and non-terminals *)
-
-  let _S = 0
-  let _x = 1
-
-  let rhss = [ [_S;_S;_x]; [_x] ]
-
-  let arr : int list array = S.mk_table rhss
-
-  let rhss' = rhss |> List.map @@ fun  x-> 
-    S.lookup_NOTE_SLOW arr x |> function Some x -> x | None -> failwith __LOC__
-
-  let new_items ~nt ~input ~k = match () with
-    | _ when nt = _S -> 
-      rhss'   (* E -> E E E | "1" | eps *)
-      |> List.map (fun bs -> let i = k in S.to_int (nt,i,k,bs))
-    | _ -> failwith __LOC__
-
-  (* NOTE input length given by command line arg *)
-  let input = String.make (Sys.argv.(1) |> int_of_string) 'x'
-
-  let parse_tm ~tm ~input ~k ~input_length = 
-    match () with
-    | _ when tm = _x -> 
-      (* print_endline (string_of_int k); *)
-      if String.get input k = 'x' then [k+1] else []
-    | _ -> failwith __LOC__
-
-  let input_length = String.length input
-
-  let init_nt = _S
-
-  let dot_bs = dot_bs' arr
-
-  let nt_item_ops = {
-    dot_nt;
-    dot_i;
-    dot_k;
-    dot_bs
-  }
-
-  let bitms_lt_k_ops = {
-    map_add=(fun k v t -> t.(k) <- v; t);
-    map_find=(fun k t -> t.(k));
-    map_empty=(Array.make (input_length + 1) map_nt_ops.map_empty);   (* FIXME +1? *)
-    map_remove=(fun k t -> failwith __LOC__);  (* not used *)
-  }
-
-  let cut = S.cut
-
-  let main () = 
-    run_earley ~nt_item_ops ~bitms_lt_k_ops ~cut ~new_items ~input ~parse_tm ~input_length ~init_nt 
-    |> fun s -> s.k |> string_of_int |> print_endline
-
-(* G2 timings:
-
-$ src $ time ./test.native 200
-8
-6
-200
-
-real	0m0.093s
-user	0m0.072s
-sys	0m0.000s
-
-
-   compare with "Directly executable Earley parsing", 2001, Fig. 8
-   (dmittedly this was rather a long time ago)
-
-*)
-
-end
-
-
-
-(* S -> SSS | SS | "b" ---------------------------------------------- *)
-
-module G3 = struct
-
-  (* 
-
-     S -> SSS | SS | "b"
-
-See Gamma_3 grammar in "Practical, general parser combinators", 2016
-
-https://cdn.rawgit.com/meerkat-parser/papers/master/pepm16.pdf
-
-  *)
-
-  open S
-
-  (* terminals and non-terminals *)
-
-  let _S = 0
-  let _S2 = 2  (* we binarize the grammar *)
-  let _b = 1
-
-  (* binarized *)
-(*
-  let rhss_S = [ [_S;_S2]; [_S2]; [_b] ]
-  let rhss_S2 = [ [_S;_S] ]
-  let rhss = (rhss_S@rhss_S2)
-
-  let arr : int list array = S.mk_table rhss
-
-  let rhss_S' = rhss_S |> List.map @@ fun  x-> 
-    S.lookup_NOTE_SLOW arr x |> function Some x -> x | None -> failwith __LOC__
-
-  let rhss_S2' = rhss_S2 |> List.map @@ fun  x-> 
-    S.lookup_NOTE_SLOW arr x |> function Some x -> x | None -> failwith __LOC__
-
-  let new_items ~nt ~input ~k = match () with
-    | _ when nt = _S -> 
-      rhss_S'  
-      |> List.map (fun bs -> let i = k in S.to_int (nt,i,k,bs))
-    | _ when nt = _S2 -> 
-      rhss_S2' 
-      |> List.map (fun bs -> let i = k in S.to_int (nt,i,k,bs))
-    | _ -> failwith __LOC__
-*)
-  
-  (* unbinarized *)
- 
-  let rhss = [ [_S;_S; _S]; [_S;_S]; [_b] ]
-
-  let arr : int list array = S.mk_table rhss
-
-  let rhss_S' = rhss |> List.map @@ fun  x-> 
-    S.lookup_NOTE_SLOW arr x |> function Some x -> x | None -> failwith __LOC__
-
-  let new_items ~nt ~input ~k = match () with
-    | _ when nt = _S -> 
-      rhss_S'  
-      |> List.map (fun bs -> let i = k in S.to_int (nt,i,k,bs))
-    | _ -> failwith __LOC__
-
-
-  (* NOTE input length given by command line arg *)
-  let input = String.make (Sys.argv.(1) |> int_of_string) 'b'
-
-  let parse_tm ~tm ~input ~k ~input_length = 
-    match () with
-    | _ when tm = _b -> 
-      (* print_endline (string_of_int k); *)
-      if String.get input k = 'b' then [k+1] else []
-    | _ -> failwith __LOC__
-
-  let input_length = String.length input
-
-  let init_nt = _S
-
-  let dot_bs = dot_bs' arr
-
-  let nt_item_ops = {
-    dot_nt;
-    dot_i;
-    dot_k;
-    dot_bs
-  }
-
-  let bitms_lt_k_ops = {
-    map_add=(fun k v t -> t.(k) <- v; t);
-    map_find=(fun k t -> t.(k));
-    map_empty=(Array.make (input_length + 1) map_nt_ops.map_empty);   (* FIXME +1? *)
-    map_remove=(fun k t -> failwith __LOC__);  (* not used *)
-  }
-
-  let cut = S.cut
-
-  let main () = 
-    run_earley ~nt_item_ops ~bitms_lt_k_ops ~cut ~new_items ~input ~parse_tm ~input_length ~init_nt 
-    |> fun s -> s.k |> string_of_int |> print_endline
-
-(* G3 timings:
-
-unbinarized: 
-
-$ src $ time ./test.native 400
-8
-6
-9
-400
-
-real	0m3.674s
-user	0m3.528s
-sys	0m0.040s
-
-   This is in a vm on a macbook air from 2013. Even so, a bit faster than
-   the paper
-
-
-binarized: no real difference :(
-
-*)
-
-end
-
-
-
-
-*)
