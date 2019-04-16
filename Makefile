14b9b42ae9c585bf0638082adb3e1fb581701ac8
Tom Ridge <tom.j.ridge@googlemail.com>
2019-04-16 17:05:38 +0100

    wip

--- Makefile
+++ Makefile
@@ -1,15 +1,27 @@
 SHELL:=bash
+ROOT:=--root=.
+DUNE:=dune
 
 all:
-	dune build @all
-	dune build @doc-private
-	dune build src/example.exe
-	echo "doc at _build/default/_doc/_html//xxx@./Earley/index.html"
+	$(DUNE) build $(ROOT) @all
+	$(MAKE) docs
+#	dune build src/example.exe
+
+
+SRC:=_build/default/_doc/_html
+DST:=docs
+docs: FORCE
+	$(DUNE) build $(ROOT) @doc
+	rm -rf $(DST)/*
+	cp -R $(SRC)/* $(DST)
+	echo "docs built"
+
 
 run_example:
-	time dune exec src/example.exe 400 # should take about 6s?
+	time dune exec $(ROOT) src/example.exe 400 # should take about 6s?
 
 clean:
-	dune clean
+	dune clean $(ROOT)
 
 
+FORCE:
--- src/earley.ml
+++ src/earley.ml
@@ -1,13 +1,46 @@
 (** An experiment to see whether the imperative code (represented using
    a monad) is easier to read; probably it is. *)
 
-open Prelude
+module type A = sig
+
+  type i_t = int  
+  type k_t = int
+  type j_t = int
+
+  type nt
+  type tm
+  type sym
+
+  type nt_item  
+
+  type nt_item_set
+
+  (* when we increase k, we need to alter the state significantly;
+     this reveals the structure of the state *)
+
+  (** nt -> nt_item_set *)
+  type bitms_at_k 
+
+  (** int -> bitms_at_k *)
+  type bitms_lt_k  
+
+  (** NOTE following are per k *)
+
+  (** int*nt set *)
+  type ixk_done  
+
+  (** tm -> j list option *)
+  type ktjs  
+
+end
+
+
 
 module Make(A:A) = struct
 
   (* to make doc self-contained *)
   include A
-      
+
   type item_ops = {
     sym_case: 'a. nt:(nt -> 'a) -> tm:(tm -> 'a) -> sym -> 'a;
     _NT: nt -> sym;
@@ -19,6 +52,12 @@ module Make(A:A) = struct
     elements : nt_item_set -> nt_item list
   }
 
+
+  (* NOTE a map from int, so we can implement straightforwardly *)
+  type todo_gt_k = (int,nt_item_set) Hashtbl.t
+
+  let todo_gt_k_find: int -> todo_gt_k -> nt_item_set = failwith "FIXME"
+
   type state = {
     todo: nt_item list;
     todo_done: nt_item_set;
@@ -29,60 +68,72 @@ module Make(A:A) = struct
     ktjs:ktjs
   }
 
+  type 'a m = state -> 'a * state
+  let ( >>= ) (a:'a m) (ab:'a -> 'b m) : 'b m = 
+    fun s ->
+      a s |> fun (a,s) -> 
+      ab a s
+  let _ = ( >>= )
+  let return a = fun s -> (a,s)
+
+
+  let with_state: (state -> state) -> unit m = fun f -> 
+    fun s -> ((),f s)
+
+  (* FIXME these expose the state type via _ m *)
+  type atomic_operations = {
+    (* FIXME this op is already known now we know 'a m *)
+    pop_todo: unit -> nt_item option m;
+
+    (* following three could be implemented as funs t -> t, then lifted to the monad *)
+    get_bitms_at_k: nt -> nt_item list m;  (* or set? *)
+    get_bitms_lt_k: int * nt -> nt_item list m;  (* or set? *)
+    add_bitm_at_k: nt_item -> nt -> unit m;  (* FIXME don't need nt *)
+
+    (* FIXME following two could be implemented here, since we know the state impl type *)
+    add_todos_at_k: nt_item list -> unit m;
+    add_todos_gt_k: nt_item list -> unit m;
+
+    (* FIXME these, and others, could be implemented outside the monad
+       as a funtion t->t, then injected into the monad now we know
+       what 'a m is *)
+    add_ixk_done: int*nt -> unit m;
+    mem_ixk_done: int*nt -> bool m;
+
+    find_ktjs: tm -> int list option m;
+    add_ktjs: tm -> int list -> unit m;
+  }
+
+
   type state_ops = {
-    todo_gt_k_find: int -> todo_gt_k -> nt_item_set;
-    update_bitms_lt_k: int -> bitms_at_k -> bitms_lt_k -> bitms_lt_k;
+    update_bitms_lt_k: int -> bitms_at_k -> bitms_lt_k -> bitms_lt_k; (* FIXME what for? *)
     empty_bitms_at_k: bitms_at_k;
     empty_ixk_done: ixk_done;
     empty_ktjs: ktjs;
   }
 
-  type monad_ops = {
-    bind: 'a 'b. 'a m -> ('a -> 'b m) -> 'b m;
-    return: 'a. 'a -> 'a m
-  }
 
-  type atomic_operations = {
-    get_bitms_at_k: nt -> nt_item list m;  (* or set? *)
-    get_bitms_lt_k: int * nt -> nt_item list m;  (* or set? *)
-    add_bitm_at_k: nt_item -> nt -> unit m;  (* FIXME don't need nt *)
-    pop_todo: unit -> nt_item option m;
-    add_todos_at_k: nt_item list -> unit m;
-    add_todos_gt_k: nt_item list -> unit m;
-    add_ixk_done: int*nt -> unit m;
-    mem_ixk_done: int*nt -> bool m;
-    find_ktjs: tm -> int list option m;
-    add_ktjs: tm -> int list -> unit m;
-    with_state: (state -> state) -> unit m;
-  }
-
-
-  module Internal = struct
+  module Internal2 = struct
 
 
     (* profiling; debugging --------------------------------------------- *)
-    let dest_Some = function Some x -> x | _ -> (failwith "dest_Some")
 
-    let now () = Core.Time_stamp_counter.(
-        now () |> to_int63 |> Core.Int63.to_int |> dest_Some)
-
-    let Tjr_profile.{mark;get_marks} = Tjr_profile.mk_profiler ~now
-
+    (* don't want to depend on other libs at this point *)
+    let _mark_ref = ref (fun (i:int) -> ())
+    let mark x = x |> !_mark_ref
 
     (* main ------------------------------------------------------------- *)
 
-    let run_earley ~monad_ops ~item_ops ~state_ops ~at_ops = 
-      let { bind; return } = monad_ops in
-      let ( >>= ) = bind in
+    let run_earley ~item_ops ~state_ops ~at_ops = 
       let { sym_case; _NT; dot_nt; dot_i; dot_k; dot_bs; cut; elements } =
         item_ops 
       in
-      let { todo_gt_k_find; update_bitms_lt_k; empty_bitms_at_k;
+      let { update_bitms_lt_k; empty_bitms_at_k;
             empty_ixk_done; empty_ktjs } = state_ops 
       in
       let { get_bitms_at_k; get_bitms_lt_k; add_bitm_at_k; pop_todo;
             add_todos_at_k; add_todos_gt_k; add_ixk_done;
-            mem_ixk_done; find_ktjs; add_ktjs; with_state } = at_ops
+            mem_ixk_done; find_ktjs; add_ktjs } = at_ops
       in
       let is_finished nitm = nitm|>dot_bs = [] in
       let module Let_syntax = struct 
@@ -258,18 +309,28 @@ discussion is indexed by these labels
   module Export : sig
     type earley_parser
     val make_earley_parser: 
-      monad_ops:monad_ops ->
       item_ops:item_ops ->
       state_ops:state_ops ->
       at_ops:atomic_operations ->
       earley_parser
 
+    val _run_earley_parser:
+      earley_parser:earley_parser -> 
+      new_items:(nt:nt -> input:'a -> k:i_t -> nt_item list) ->
+      input:'a ->
+      input_length:i_t -> 
+      parse_tm:(tm:tm -> input:'a -> k:i_t -> input_length:i_t -> i_t list) ->
+      unit m
+
+    (** returns the final state *)
     val run_earley_parser:
       earley_parser:earley_parser -> 
       new_items:(nt:nt -> input:'a -> k:i_t -> nt_item list) ->
       input:'a ->
+      input_length:i_t -> 
       parse_tm:(tm:tm -> input:'a -> k:i_t -> input_length:i_t -> i_t list) ->
-      input_length:i_t -> unit m
+      initial_items:nt_item list ->
+      state
   end = struct
     type earley_parser = {
       run_parser: 'a. new_items:(nt:nt -> input:'a -> k:i_t -> nt_item list) ->
@@ -278,10 +339,34 @@ discussion is indexed by these labels
       input_length:i_t -> unit m
     }
 
-    let make_earley_parser ~monad_ops ~item_ops ~state_ops ~at_ops =
-      {run_parser=fun ~new_items -> Internal.run_earley ~monad_ops ~item_ops ~state_ops ~at_ops ~new_items}
+    let make_earley_parser ~item_ops ~state_ops ~at_ops =
+      {run_parser=fun ~new_items -> Internal2.run_earley ~item_ops ~state_ops ~at_ops ~new_items}
 
-    let run_earley_parser ~earley_parser = earley_parser.run_parser
+    let _run_earley_parser ~earley_parser 
+        ~new_items
+        ~input 
+        ~input_length
+        ~parse_tm
+      = earley_parser.run_parser
+        ~new_items
+        ~input 
+        ~input_length
+        ~parse_tm
+
+    let run_earley_parser ~earley_parser
+        ~new_items
+        ~input 
+        ~input_length
+        ~parse_tm
+        ~initial_items =
+      let initial_state = failwith "" in
+      _run_earley_parser ~earley_parser 
+        ~new_items
+        ~input 
+        ~input_length
+        ~parse_tm
+        initial_state
+      |> fun ((),s) -> s
   end
 
   include Export
